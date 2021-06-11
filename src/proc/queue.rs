2ae8dfd0967bb555ed18d04dd4cad8a8868c4eaa
JIceberg <jisenberg3@gatech.edu>
2021-06-10 20:47:16 -0400

    add newborn task queue

---
+++ src/proc/queue.rs
@@ -0,0 +1,42 @@
+// a task priority queue that holds all born tasks in system
+
+use super::task::Task;
+use std::collections::HashMap;
+
+pub struct TaskQueue {
+    tasks: HashMap<u128, Vec<Task>>,
+}
+
+impl TaskQueue {
+    pub fn new() -> Self {
+        Self { tasks: HashMap::new() }
+    }
+
+    pub fn add(&mut self, task: Task) {
+        let start_time = task.get_start_time();
+        if !self.tasks.contains_key(&start_time) {
+            self.tasks.insert(start_time, Vec::new());
+        }
+        self.tasks.get_mut(&start_time)
+            .unwrap()
+            .push(task);
+    }
+
+    pub fn pop(&mut self) -> Vec<Task> {
+        if self.is_empty() {
+            return Vec::new();
+        }
+        let mut smallest_key = 0;
+        for key in self.tasks.keys() {
+            if smallest_key > *key || smallest_key == 0 {
+                smallest_key = *key;
+            }
+        }
+        
+        self.tasks.remove(&smallest_key).unwrap()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.tasks.is_empty()
+    }
+}
---
+++ tests/queue_test.rs
@@ -0,0 +1,42 @@
+#![cfg(test)]
+
+extern crate rust_cfs as cfs;
+
+use cfs::proc::task::{Task, TaskStatus};
+use cfs::proc::queue::TaskQueue;
+
+#[test]
+fn test_queue() {
+    let task_one    = Task::new(1, 13191, 10, 5, 1);
+    let task_two    = Task::new(2, 13289, 10, 5, 1);
+    let task_three  = Task::new(3, 139, 10, 5, 2);
+    let task_four   = Task::new(4, 31921, 5, 10, 3);
+    let task_five   = Task::new(5, 3874, 7, 3, 5);
+    let task_six    = Task::new(6, 17013, 10, 6, 5);
+
+    let mut task_queue = TaskQueue::new();
+    
+    task_queue.add(task_one);
+    task_queue.add(task_two);
+    task_queue.add(task_three);
+    task_queue.add(task_four);
+    task_queue.add(task_five);
+    task_queue.add(task_six);
+
+    let mut idx = 1;
+    while !task_queue.is_empty() {
+        let tasks = task_queue.pop();
+        for mut task in tasks {
+            task.schedule();
+            assert_eq!(task.get_start_time(), idx);
+            assert_eq!(task.get_status(), TaskStatus::Waiting);
+        }
+        idx = match idx {
+            1 => 2,
+            2 => 3,
+            3 => 5,
+            _ => 0
+        };
+    }
+    assert_eq!(idx, 0);
+}
