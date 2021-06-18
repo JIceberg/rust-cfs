aaa490a22d3ad7ca050cd959d9c58057143f855b
JIceberg <jisenberg3@gatech.edu>
2021-06-18 10:46:36 -0400

    change task generation script to create better tasks for simulation

--- generate_tasks.py
+++ generate_tasks.py
@@ -5,16 +5,17 @@ task_file = open("tasks.txt", "x")
 
 import random
 
-num_tasks = random.randint(0, 2 ** 4)
+num_tasks = random.randint(1, 2 ** 4)
 
 tasks = []
 for i in range(num_tasks):
+    max_cpu_time = random.randint(1, 2 ** 20)
     tasks.append(
         (
-            random.randint(0, 2 ** 20),
-            random.randint(0, 2 ** 16),
-            random.randint(0, 2 ** 16),
-            random.randint(0, 2 ** 16)
+            max_cpu_time,
+            random.randint(1, min(2 ** 16, max_cpu_time)),
+            random.randint(0, 2 ** 11),
+            random.randint(1, 2 ** 5)
         )
     )
 
